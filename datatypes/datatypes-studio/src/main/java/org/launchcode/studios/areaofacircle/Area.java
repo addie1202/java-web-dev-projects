59492219c6c91c8708c52aa6f91745e874187106
addie1202 <adlai.gaitherganim@gmail.com>
2023-08-19 18:08:15 -0500

    finished circle class

--- datatypes/datatypes-studio/src/main/java/org/launchcode/studios/areaofacircle/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/studios/areaofacircle/Area.java
@@ -7,7 +7,7 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius");
         double radius = input.nextDouble();
-        Circle.getArea(Double radius);
+        double area = Circle.getArea(radius);
         System.out.println(String.format("The area of a circle of radius %s is %s", radius, area));
     }
 }
